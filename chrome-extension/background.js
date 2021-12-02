d4e161ff27f08cac36d8432825e69b2d4c106e78
Cormac Muir <2375400m@Student.gla.ac.uk>
2021-12-02 01:45:28 +0000

    need to resolve unique url entries

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,18 +1,40 @@
-import { tracker_domains } from './trackerlist.mjs'
-chrome.runtime.onInstalled.addListener(function() {
+
+chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.local.set({ 'totalAds': 0 });
 });
 
+var filter = { urls: ["<all_urls>"] };
+
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+    
+    var match = details.url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    var domain = match && match[1];
+    
+    var unique_domains = []
+    
 
 
-chrome.tabs.onUpdated.addListener(function(tabId, info) {
+    if(domain){
+        if(unique_domains.indexOf(domain) === -1) {
+            unique_domains.push(domain);
+            alert(domain);}
+        
+    }
+
+}, filter);
+
+
+
+
+
+chrome.tabs.onUpdated.addListener(function (tabId, info) {
 
     if (info.status === 'complete') {
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
 
             let url = tabs[0].url;
             alert(url)
-            alert(tracker_domains[0])
+
 
         });
 
