fbcab2f560ae1cf51dc1183ee3a29aa992afb7b6
Cormac <2375400m@student.gla.ac.uk>
2022-01-10 17:50:02 +0000

    finished single player version and fixed error message bug

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,6 +1,6 @@
 //this needs to change, these should be information that will be sent to server.
 //Dont want a new adCount ONLY when the user installs the app
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.set({ 'adCount': 0 });
 });
 
@@ -9,20 +9,21 @@ var unique_hosts = [];
 
 
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    chrome.storage.local.get("turn", function (g) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.storage.local.get("turn", function(g) {
         if (changeInfo.status == 'loading') {
             let url = new URL(tab.url)
             let domain = url.hostname
-            chrome.storage.local.get("domainHistory", function (f) {
-                if (f.domainHistory.length < g.turn) {
+            chrome.storage.local.get("domainHistory", function(f) {
+                if (f.domainHistory.length < g.turn && domain != "newtab") {
                     if (f.domainHistory.includes(domain)) {
                         chrome.storage.local.set({ 'error': "true" });
-                    }
-                    else {
+                    } else {
                         f.domainHistory.push(domain);
                         chrome.storage.local.set({ "domainHistory": f.domainHistory });
                         chrome.storage.local.set({ 'currentURL': domain });
+                        chrome.storage.local.set({ 'waiting': "valid" });
+                        chrome.storage.local.remove("error");
                         unique_hosts = [];
                     }
                 } else {
@@ -34,43 +35,43 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 })
 
 
-chrome.webRequest.onBeforeRequest.addListener(function (details) {
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    chrome.storage.local.get("waiting", function(f) {
+        if (f.waiting != "complete") {
 
-    var match = details.url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-    var domain = match[0] && match[1];
-    var hostname = domain.substring(0, domain.lastIndexOf('.'));
-    if (domain != null) {
-        if (unique_hosts.indexOf(hostname) === -1) {
-            unique_hosts.push(hostname);
-            if (hostname in tracker_domains || domain in tracker_domains) {
-                chrome.storage.local.get("waiting", function (f) {
-                    if (Date.now() < f.waiting) {
-                        chrome.storage.local.get("adCount", function (f) {
+            var match = details.url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+            var domain = match[0] && match[1];
+            var hostname = domain.substring(0, domain.lastIndexOf('.'));
+            if (domain != null) {
+                if (unique_hosts.indexOf(hostname) === -1) {
+                    unique_hosts.push(hostname);
+                    if (hostname in tracker_domains || domain in tracker_domains) {
+
+                        chrome.storage.local.get("adCount", function(f) {
                             chrome.storage.local.set({ "adCount": f.adCount + 1 })
                         })
                         console.log("AD TRACKER FOUND! " + hostname);
-                    }else if(Date.now() > f.waiting){
-                        chrome.storage.local.remove("waiting");
-                    }
-                })
 
+
+                    }
+                }
             }
+        } else {
+            chrome.storage.local.remove("waiting");
         }
-    }
+
+    })
 
 }, filter);
 
 
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-    for (var key in changes) {
-        if (key == "currentURL") {
-            if (changes["currentURL"].newValue != null) {
-                chrome.storage.local.set({ "waiting": (Date.now() + 5000) });
-            }
+// chrome.storage.onChanged.addListener(function(changes, namespace) {
+//     for (var key in changes) {
+//         if (key == "currentURL") {
+//             if (changes["currentURL"].newValue != null) {
+//                 chrome.storage.local.set({ "waiting": (Date.now() + 5000) });
+//             }
 
-        }
-    }
-});
-
-
-
+//         }
+//     }
+// });
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,43 +1,40 @@
-chrome.storage.onChanged.addListener(function (changes, namespace) {
+chrome.storage.onChanged.addListener(function(changes, namespace) {
     for (var key in changes) {
         if (key == "adCount") {
             document.getElementById("adCount").innerHTML = changes['adCount'].newValue;
-        }
-        else if(key=="waiting"){
-            if(!changes['waiting'].newValue){
-                window.location.reload();
-            }
+        } else if (key == "waiting") {
+            window.location.reload();
         }
 
     }
 });
 
-document.getElementById("newGame").addEventListener("click", function () {
+document.getElementById("newGame").addEventListener("click", function() {
     console.log("SETTING UP GAME!");
     setupGame();
 });
 
 
 
-document.getElementById("stick").addEventListener("click", function () {
+document.getElementById("stick").addEventListener("click", function() {
     checkGameOutcome();
 });
 
-document.getElementById("twist").addEventListener("click", function () {
+document.getElementById("twist").addEventListener("click", function() {
     chrome.storage.local.set({ "waiting": "true" })
-    chrome.storage.local.get("turn", function (f) {
+    chrome.storage.local.get("turn", function(f) {
         chrome.storage.local.set({ "turn": f.turn + 1 });
     })
     chrome.storage.local.remove("currentURL");
-    chrome.storage.local.remove("error");
+
     window.location.reload();
 });
 
 
 function checkGameOutcome() {
-    chrome.storage.local.get("adCount", function (f) {
-        chrome.storage.local.get("botScore", function (g) {
-            chrome.storage.local.get("goal", function (h) {
+    chrome.storage.local.get("adCount", function(f) {
+        chrome.storage.local.get("botScore", function(g) {
+            chrome.storage.local.get("goal", function(h) {
                 let adCount = f.adCount;
                 let botScore = g.botScore;
                 let goal = h.goal;
@@ -58,6 +55,7 @@ function checkGameOutcome() {
     window.location.reload();
 
 }
+
 function setupGame() {
     console.log("SETTING UP GAME!");
     let randomGoal = Math.floor(Math.random() * 25 + 5);
@@ -72,13 +70,13 @@ function setupGame() {
     chrome.storage.local.set({ 'domainHistory': [] });
 
     window.location.reload();
-    
+
 
 }
 
 
 
-window.onload = function () {
+window.onload = function() {
     refreshPopup();
 }
 
@@ -87,50 +85,50 @@ window.onload = function () {
 function refreshPopup() {
     $('.resultScreen').hide(0);
     $('#error').hide();
-    chrome.storage.local.get("gameResult", function (f) {
+    chrome.storage.local.get("gameResult", function(f) {
         if (f.gameResult != "inProgress") {
 
             showResultScreen(f.gameResult);
 
         } else {
-            
 
-            chrome.storage.local.get("waiting", function (f) {
+
+            chrome.storage.local.get("waiting", function(f) {
                 if (f.waiting) {
                     $('.gameButtons').hide(0);
-                    
-                    
 
-                }else{
+
+
+                } else {
                     $('.awaitingResponse').hide(0);
                 }
             })
 
-            chrome.storage.local.get("adCount", function (f) {
+            chrome.storage.local.get("adCount", function(f) {
                 document.getElementById("adCount").innerHTML = f.adCount;
             })
 
-            chrome.storage.local.get("goal", function (f) {
+            chrome.storage.local.get("goal", function(f) {
                 document.getElementById("goal").innerHTML = f.goal;
             })
 
 
-            chrome.storage.local.get("turn", function (f) {
+            chrome.storage.local.get("turn", function(f) {
                 if (f.turn == 0) {
                     document.getElementById("stick").disabled = true;
                 }
             })
-            chrome.storage.local.get("error", function (f) {
+            chrome.storage.local.get("error", function(f) {
 
 
-                if (f.error=="true") {
+                if (f.error == "true") {
                     console.log("showing error");
                     $('#error').show();
-                    
+
                 }
             })
 
-            chrome.storage.local.get("currentURL", function (f) {
+            chrome.storage.local.get("currentURL", function(f) {
 
                 if (f.currentURL) {
                     document.getElementById("currentURL").innerHTML = f.currentURL;
@@ -146,7 +144,7 @@ function refreshPopup() {
         $('.resultScreen').show(0);
         $('.gameScreen').hide(0);
         document.getElementById("result").innerHTML = gameResult;
-        chrome.storage.local.get("adCount", function (f) {
+        chrome.storage.local.get("adCount", function(f) {
             document.getElementById("finalScore").innerHTML = f.adCount;
         })
 
@@ -157,6 +155,6 @@ function refreshPopup() {
 
 
 //ADMIN BUTTON- REMOVE FOR PROD
-document.getElementById("admin").addEventListener("click", function () {
+document.getElementById("admin").addEventListener("click", function() {
     setupGame();
 });
