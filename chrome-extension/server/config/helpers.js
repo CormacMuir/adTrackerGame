10745d71075731e9ce205dfb298f4fbe9393b3e2
Cormac <2375400m@student.gla.ac.uk>
2022-03-26 20:02:02 +0000

    fixed handling for user disconnect

--- chrome-extension/server/config/helpers.js
+++ chrome-extension/server/config/helpers.js
@@ -2,11 +2,11 @@ const mysqli = require('mysqli');
 const conn = new mysqli({
     //MySQL db connection details
 
-    host: 'us-cdbr-east-05.cleardb.net',
-    post: '3386',
-    user: 'b03851587ae154',
-    password: '5cb8ad8a',
-    database: 'heroku_6e85a026a1900c7'
+    host: 'X',
+    post: 'X',
+    user: 'X',
+    password: 'X',
+    database: 'X'
 });
 let db = conn.emit(false, '');
 
